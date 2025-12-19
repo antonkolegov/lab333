0559bec54d5bc1483666fbd0dcb41b5fc1da9201
Антон Колегов <kolegov961@gmail.com>
2025-12-19 20:39:58 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,16 @@
+# Размеры окна 
+WINDOW_WIDTH = 80
+WINDOW_HEIGHT = 25
+
+# Цвета 
+COLORS = {
+    'reset': '\033[0m',
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'magenta': '\033[35m',
+    'cyan': '\033[36m',
+}
+
+DEBUG = False
